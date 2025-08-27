8ea3620ba2d3ce804528e76cf6b62d9416234fa8
AndrewWillis7 <andrewwillis771@outlook.com>
2025-08-26 17:26:03 -0700

    output fixes

--- src/gpt_pipeline/output.py
+++ src/gpt_pipeline/output.py
@@ -6,5 +6,5 @@ def line(prompt, dataset, device_model):
     output_tokens = device_model.generate(context, max_new_tokens=300)[0].tolist()
     output_text = dataset.decode(output_tokens)
 
-    print(prompt)
+    # print(prompt)
     print(output_text)
